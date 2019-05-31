10429a989f7d141392281829a9807b2c8dbfecd0
iningwei <iningwei@outlook.com>
2019-05-31 11:09:03 +0800

    add rotation classes,and InfiniteTimeAction.cs

---
+++ Assets/Scripts/UnityCCAction/InfiniteTimeAction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZGame.cc
+{
+    public class InfiniteTimeAction : Action
+    {
+        public override Action Clone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Finish()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override GameObject GetOriginalTarget()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override GameObject GetTarget()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsDone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetTarget(GameObject target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
