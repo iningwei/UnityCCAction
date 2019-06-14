76889eae73192f807854f67b8e93c3176855dd8e
iningwei <iningwei@outlook.com>
2019-06-14 11:28:08 +0800

    make TestAction.cs's OnGUI() more neat

--- Assets/Scripts/TestAction.cs
+++ Assets/Scripts/TestAction.cs
@@ -37,7 +37,7 @@ public class TestAction : MonoBehaviour
 
     private void OnGUI()
     {
-        if (GUI.Button(new Rect(10, 10, 120, 40), "延迟调用"))
+        if (GUI.Button(new Rect(10, 10, 80, 25), "延迟调用"))
         {
             this.gameObject.RunAction(new CallFunc((a) =>
             {
@@ -46,8 +46,8 @@ public class TestAction : MonoBehaviour
         }
 
 
-
-        if (GUI.Button(new Rect(10, 70, 120, 40), "移动"))
+        #region 移动
+        if (GUI.Button(new Rect(10, 50, 70, 25), "移动"))
         {
             this.gameObject.RunAction(new MoveTo(3, new Vector3(2, 3, 0)).OnComplete((a) =>
             {
@@ -55,21 +55,22 @@ public class TestAction : MonoBehaviour
                 this.Reset();
             }).SetTag(999).SetRepeatTimes(3));
         }
-        if (GUI.Button(new Rect(150, 70, 100, 40), "暂停移动"))
+        if (GUI.Button(new Rect(90, 50, 70, 25), "暂停移动"))
         {
             this.gameObject.PauseAction(999);
         }
-        if (GUI.Button(new Rect(260, 70, 100, 40), "恢复移动"))
+        if (GUI.Button(new Rect(170, 50, 70, 25), "恢复移动"))
         {
             this.gameObject.ResumeAction(999);
         }
+        #endregion
 
 
 
 
 
 
-        if (GUI.Button(new Rect(10, 130, 120, 40), "缩放"))
+        if (GUI.Button(new Rect(10, 90, 70, 25), "缩放"))
         {
             this.gameObject.RunAction(new ScaleTo(3, new Vector3(2, 2, 0)).SetRepeatTimes(2).OnComplete((a) =>
             {
@@ -77,22 +78,33 @@ public class TestAction : MonoBehaviour
             }));
         }
 
-        if (GUI.Button(new Rect(10, 190, 120, 40), "旋转"))
+
+        #region 旋转
+        if (GUI.Button(new Rect(10, 130, 70, 25), "旋转"))
         {
             this.gameObject.RunAction(new Rotate(60, 0, 0, Space.Self).SetTag(100));
         }
 
-
-
-
-
-        if (GUI.Button(new Rect(10, 250, 120, 40), "移除 旋转"))
+        if (GUI.Button(new Rect(90, 130, 70, 25), "暂停旋转"))
+        {
+            this.gameObject.PauseAction(100);
+        }
+        if (GUI.Button(new Rect(170, 130, 70, 25), "恢复旋转"))
+        {
+            this.gameObject.ResumeAction(100);
+        }
+        if (GUI.Button(new Rect(250, 130, 70, 25), "移除 旋转"))
         {
             this.gameObject.RemoveAction(100);
         }
+        #endregion
 
 
-        if (GUI.Button(new Rect(10, 310, 120, 40), "移除所有Action"))
+
+
+
+
+        if (GUI.Button(new Rect(10, 170, 120, 25), "移除所有Action"))
         {
             this.gameObject.RemoveAllActions();
         }
